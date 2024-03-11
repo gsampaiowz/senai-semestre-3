ff9a2842469cd39b894ef17eadbb0174a53645b4
gsampaiowz <gabrielsampaio1216@gmail.com>
2024-03-11 17:23:31 -0300

    expo maps

---
+++ mapsExemplo/App.js
@@ -0,0 +1,314 @@
+import { StatusBar } from "expo-status-bar";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+
+import {
+  requestForegroundPermissionsAsync, // Solicita a permissão de localização
+  getCurrentPositionAsync, // Pega a posição atual do usuário
+} from "expo-location";
+import { useEffect, useState } from "react";
+import MapViewDirections from "react-native-maps-directions";
+import { mapskey } from "./utils/mapsKey";
+
+export default function App() {
+  const [initialPosition, setInitialPosition] = useState(null);
+
+  async function CapturarLocalizacao() {
+    const { granted } = await requestForegroundPermissionsAsync();
+
+    if (granted) {
+      const currentPosition = await getCurrentPositionAsync();
+
+      await setInitialPosition(currentPosition);
+
+      console.log(initialPosition);
+    }
+  }
+
+  useEffect(() => {
+    CapturarLocalizacao();
+  }, [10000]);
+
+  return (
+    <View style={styles.container}>
+      {initialPosition != null ? (
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: initialPosition.coords.latitude,
+            longitude: initialPosition.coords.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          provider={PROVIDER_GOOGLE}
+          customMapStyle={grayMapStyle}
+        >
+          {/* Criando um marcador no mapa */}
+          <Marker
+            coordinate={{
+              latitude: initialPosition.coords.latitude,
+              longitude: initialPosition.coords.longitude,
+            }}
+            title="Senai Paulo Skaf"
+            description="Melhor escola de tecnologia do Brasil"
+            pinColor="purple"
+          />
+
+          <MapViewDirections
+            origin={initialPosition.coords}
+            destination={{
+              latitude: -23.686936,
+              longitude: -46.4457019,
+            }}
+            apikey={mapskey}
+            strokeWidth={5}
+            strokeColor="purple"
+
+          />
+        </MapView>
+      ) : (
+        <>
+          <Text>Localização não encontrada</Text>
+
+          <ActivityIndicator size={64} />
+        </>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    flex: 1,
+    width: "100%",
+  },
+});
+
+const grayMapStyle = [
+  {
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#E1E0E7",
+      },
+    ],
+  },
+  {
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        saturation: -5,
+      },
+      {
+        lightness: -5,
+      },
+    ],
+  },
+  {
+    elementType: "labels.icon",
+    stylers: [
+      {
+        visibility: "on",
+      },
+    ],
+  },
+  {
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#FBFBFB",
+      },
+    ],
+  },
+  {
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#33303E",
+      },
+    ],
+  },
+  {
+    featureType: "administrative",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.country",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.land_parcel",
+    stylers: [
+      {
+        visibility: "on",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.locality",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+  {
+    featureType: "poi.business",
+    stylers: [
+      {
+        visibility: "on",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#66DA9F",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text",
+    stylers: [
+      {
+        visibility: "on",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#1B1B1B",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    stylers: [
+      {
+        visibility: "on",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#C6C5CE",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#FBFBFB",
+      },
+    ],
+  },
+  {
+    featureType: "road.arterial",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#ACABB7",
+      },
+    ],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#8C8A97",
+      },
+    ],
+  },
+  {
+    featureType: "road.highway.controlled_access",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#8C8A97",
+      },
+    ],
+  },
+  {
+    featureType: "road.local",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+  {
+    featureType: "transit",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+  {
+    featureType: "water",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#8EA5D9",
+      },
+    ],
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#fbfbfb",
+      },
+    ],
+  },
+];
